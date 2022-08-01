f0d45b3f68544a03865b0d0af90a2e3c1389944f
Denys <ss.set2013@gmail.com>
2022-08-01 13:18:38 +0300

    first:

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.header_lang_active').click(function() {
+    $('.header_lang_select').toggle();
+    $('.header_lang_active svg').toggleClass('img_rotate');
+})
+
+$('#hw-slide-msg-close').click(function() {
+    $('.nav-toggle-wrap').addClass('mt0');
+})
